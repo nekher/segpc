dab50cdc832a6ec5d7959de065811ed2818ca354
nekher <max.mym@gmail.com>
2022-01-18 19:10:37 -0300

    1

--- crear-equipo.php
+++ crear-equipo.php
@@ -1,59 +1,75 @@
 <?php
 
-if (isset($_POST['submit'])){
+$numeropc = $_POST['numeropc'];
+$piso = $_POST['piso'];
+$area = $_POST['area'];
+$interno = $_POST['interno'];
+$usuario = $_POST['usuario'];
+$problema = $_POST['problema'];
+$serial = $_POST['serial'];
 
-}
-if(empty($_POST['ID'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['ID']);
-    }
+//database connection
 
-if(empty($_POST['PISO'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars( $_POST['PISO']);
-    }
-
-if(empty($_POST['AREA'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['AREA']);
-    }
-
-if(empty($_POST['INTERNO'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['INTERNO']);
-    }
-
-if(empty($_POST['USUARIO'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['USUARIO']);
-    }
-
-if(empty($_POST['PROBLEMA'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['PROBLEMA']);
+$conn = new mysqli('localhost', 'root', '', 'segpc');
+if($conn->connect_error){
+    die ('fallo la conexion con la base de datos : ' .$conn->connect_error);
+    }    else {
+        $stmt = $conn->prepare("ingresar en el registro (numeropc, piso, area, interno, usuario, problema, serial)
+        values (?,?,?,?,?,?,?,)");
+        $stmt->bind_param("sisisss", $numeropc, $piso, $area, $interno, $usuario, $problema, $serial);
+        $stmt->execute();
+        echo "Excelsior";
+        $stmt->close();
+        $conn->close();
     }
 
 
-if(empty($_POST['SERIAL'])){
-    echo 'NECESITAS PONER LOS DATOS PA!';
-}   
-    else {
-        echo htmlspecialchars($_POST['SERIAL']);
-    }
-
-
+//if (isset($_POST['submit'])){
+//}
+//if(empty($_POST['numeropc'])){
+//    echo 'te falto el numero de pc!';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['numeropc']);
+//    }
+//if(empty($_POST['piso'])){
+//    echo 'Te olvidaste el piso!!!';
+//}   
+//    else {
+//        echo htmlspecialchars( $_POST['piso']);
+//    }
+//if(empty($_POST['area'])){
+//    echo 'Emm.. y el area?!';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['area']);
+//    }
+//if(empty($_POST['interno'])){
+//    echo 'Se te ocurrio que necesitamos comunicarnos? cual es el interno?!';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['interno']);
+//    }
+//if(empty($_POST['usuario'])){
+//    echo 'Por quien preguntamos?!';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['usuario']);
+//    }
+//if(empty($_POST['problema'])){
+//    echo 'Cual es el problema?!';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['problema']);
+//    }
+//
+//if(empty($_POST['serial'])){
+//    echo 'Numero de Serie. !';
+//}   
+//    else {
+//        echo htmlspecialchars($_POST['serial']);
+//    }
+//
 
 ?>
 <!DOCTYPE html>
@@ -72,31 +88,31 @@ if(empty($_POST['SERIAL'])){
  
     <form action="crear-equipo.php" method="POST">
       <div class="form-item">
-        <input type="text" class="form-input" name="ID" placeholder="ID">
+        <input type="text" class="form-input" name="numeropc" placeholder="Numero de PC">
       </div>
 
       <div class="form-item">
-        <input type="text" class="form-input" name="PISO" placeholder="PISO">
+        <input type="text" class="form-input" name="piso" placeholder="PISO">
       </div>
       
       <div class="form-item">
-        <input type="text" class="form-input" name="AREA" placeholder="AREA">
+        <input type="text" class="form-input" name="area" placeholder="AREA">
       </div>
 
       <div class="form-item">
-        <input type="text" class="form-input" name="INTERNO" placeholder="INTERNO">
+        <input type="text" class="form-input" name="interno" placeholder="INTERNO">
       </div>
       
       <div class="form-item">
-        <input type="text" class="form-input" name="USUARIO" placeholder="USUARIO">
+        <input type="text" class="form-input" name="usuario" placeholder="USUARIO">
       </div>
 
       <div class="form-item">
-        <input type="text" class="form-input" name="PROBLEMA" placeholder="PROBLEMA">
+        <input type="text" class="form-input" name="problema" placeholder="PROBLEMA">
       </div>
 
       <div class="form-item">
-        <input type="text" class="form-input" name="SERIAL" placeholder="SERIAL">
+        <input type="text" class="form-input" name="serial" placeholder="SERIAL">
       </div>
 
 
